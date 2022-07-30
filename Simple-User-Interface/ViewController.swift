517be2d6db7fd5cd1fb2b917d32feb41517e4bda
Kateridzhe <ekaterina.d.volkova@gmail.com>
2022-07-30 17:13:39 +0300

    Initial Commit

---
+++ Simple-User-Interface/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Simple-User-Interface
+//
+//  Created by Ekaterina Volkova on 30/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
