86458dc13e3e6a7663259cc40fef11cb42541629
Kateridzhe <ekaterina.d.volkova@gmail.com>
2022-07-31 14:46:32 +0300

    Format numbers

--- Simple-User-Interface/TableViewCell.swift
+++ Simple-User-Interface/TableViewCell.swift
@@ -14,7 +14,7 @@ struct cellData {
 }
 
 class TableViewCell: UITableViewCell {
-
+    
     @IBOutlet weak var result: UILabel!
     @IBOutlet weak var calculateButton: UIButton!
     @IBOutlet weak var secondOperand: UILabel!
@@ -26,20 +26,32 @@ class TableViewCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         firstOperand.text = "1.0000"
+        secondOperand.text = "1.0000"
         result.text = "1.0000"
-        
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
     }
     
     @IBAction func sliderDidSlide(_ sender: UISlider){
-        let value = sender.value
+        let value = String(format: "%.4f", sender.value)
         secondOperand.text = "\(value)"
     }
     
-    @objc func didTapButton(){
+    @IBAction func stepperDidStep(_ sender: UIStepper){
+        let value = String(format: "%.4f", sender.value)
+        firstOperand.text = "\(value)"
+    }
+    
+    @IBAction func didTapButton(_ sender: UIButton){
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.maximumFractionDigits = 4
         
+        let value = Double(firstOperand.text!)! * Double(secondOperand.text!)!
+        let number = NSNumber(value: value)
+        let formattedValue = formatter.string(from: number)!
+        result.text = "\(formattedValue)"
     }
 }
